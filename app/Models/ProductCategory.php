2721bce408bb51ccdf0a5169c4e6fd6dfd789022
Syafii <syafiisyafiiahmad02@gmail.com>
2021-10-01 12:34:57 +0700

    pertama

---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ProductCategory extends Model
+{
+    use HasFactory, SoftDeletes;
+
+      /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'username',
+        'phone',
+        'roles',
+        'password',
+    ];
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'categories_id', 'id');
+    }
+}
